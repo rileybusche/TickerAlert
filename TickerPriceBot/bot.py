77242e74a006f25c53c8d25a7205217a2a4baed6
rileybusche <riley.busche@gmail.com>
2019-07-18 07:05:53 -0500

    Added more functions to the bot. Converted hard-coded keys it files to be read in.

--- TickerPriceBot/bot.py
+++ TickerPriceBot/bot.py
@@ -1,13 +1,17 @@
-# Work with Python 3.6
+
+from discord.ext import commands
+from apscheduler.schedulers.blocking import BlockingScheduler
 import discord
 import ticker_alert
-from discord.ext import commands
+import os.path
+
 
 bot = commands.Bot(command_prefix='!')
+TOKEN = open("token.txt", "r").read()
 
-# @client.event
-# async def on_ready():
-#     print("The bot is ready!")
+# Scheduler for output of ticker info
+sched = BlockingScheduler()
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -15,32 +19,132 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+# Outputs price of ticker
 @bot.command()
-async def Price(ctx, ticker: str):
-    await ctx.send(ticker_alert.callAPI(ticker))
+async def Price_Stock(ctx, ticker: str):
+    await ctx.send(ticker_alert.callStockAPI(ticker, "single"))
 
+# Outputs price of ticker
 @bot.command()
-async def Add(ctx):
-    await ctx.send("This command isn't implemented yet")
+async def Price_Crypto(ctx, crypto: str):
+    await ctx.send(ticker_alert.callCryptoAPI(crypto, "single"))
 
-@bot.command()
-async def List(ctx):
-    await ctx.send("This command isn't implemented yet")
-
+# Lists price for all tracked tickers
 @bot.command()
 async def All(ctx):
-    await ctx.send("This command isn't implemented yet")
+    await ctx.send(f"```\nAll Tracked Tickers:\n```" + ticker_alert.callStockAPI("null", "all"))
+
+# Add to list of tracked tickers (tickers.txt)
+@bot.command()
+async def Add_Stock(ctx, ticker):
+    if os.path.exists("tickers.txt"):
+        mode = "a"
+    else:
+        mode = "w"
+
+    ticker_list_contents = ticker_list = [line.rstrip('\n') for line in open('tickers.txt')]
+    for check_ticker in ticker_list_contents:
+        if ticker.lower() == check_ticker.lower():
+            ticker_in_file = True
+        else:
+            ticker_in_file = False
+
+    if not ticker_in_file:
+    # Ticker not in list
+        ticker_file = open('tickers.txt', 'a')
+        ticker_file.write(ticker + "\n")
+        await ctx.send(f"```fix\nAdding {ticker} \n" + ticker_alert.callStockAPI(ticker, "single") + "```")
+    else:
+        # Ticker already in list
+        await ctx.send(f"```fix\n{ticker} already tracked. \n" + ticker_alert.callStockAPI(ticker, "single") + "```")
+
+# Add to list of tracked cryptos (cryptos.txt)
+@bot.command()
+async def Add_Crypto(ctx, crypto):
+    try:
+        crypto_price = ticker_alert.callCryptoAPI(crypto, "single")
+    except:
+        await ctx.send(f"{crypto} is not a valid crypto ticker.")
+        return
+
+    if os.path.exists("cryptos.txt"):
+        mode = "a"
+        crypto_list_contents = crypto_list = [line.rstrip('\n') for line in open('cryptos.txt')]
+        for check_crypto in crypto_list_contents:
+            if crypto.lower() == check_crypto.lower():
+                crypto_in_file = True
+        else:
+            crypto_in_file = False
+    else:
+        mode = "w"
+    print("got to here")
+    if not crypto_in_file:
+        # Ticker not in list
+        crypto_file = open('cryptos.txt', mode)
+        crypto_file.write(crypto + "\n")
+        await ctx.send(f"```fix\nAdding {crypto} \n" + ticker_alert.callStockAPI(crypto, "single") + "```")
+    else:
+        # Ticker already in list
+        await ctx.send(f"```fix\n{crypto} already tracked. \n" + ticker_alert.callStockAPI(crypto, "single") + "```")
+
+# Remove 
+@bot.command()
+async def Remove(ctx, ticker):
+    await ctx.send("This command isn't implemented yet.")
+
+# Clears all tickers
+@bot.command()
+async def Clear_All(ctx):
+    await ctx.send("This command isn't implemented yet.")
+
+# Lists all tracked tickers (tickers.txt)
+@bot.command()
+async def List_Stocks(ctx):
+    ticker_list_contents = ticker_list = [line.rstrip('\n') for line in open('tickers.txt')]
+    message = ""
+    for ticker in ticker_list:
+        message += (str(ticker) + "\n")
+    await ctx.send(f"```fix\nTracked tickers: \n{message}```")
+
+# Lists all tracked cryptos (cryptos.txt)
+@bot.command()
+async def List_Cryptos(ctx):
+    crypto_list_contents = crypto_list = [line.rstrip('\n') for line in open('cryptos.txt')]
+    message = ""
+    for crypto in crypto_list:
+        message += (str(crypto) + "\n")
+    await ctx.send(f"```fix\nTracked cryptos: \n{message}```")
 
 @bot.command()
-async def Bot_quit(ctx):
+async def Bot_Quit(ctx):
     await bot.close()
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#     if message.content == "Hello":
-#         await message.channel.send(ticker_alert.callAPI())
 
+# Setting frequency for All() to be run
+@bot.command()
+async def Set_Timer(ctx, time):
+    if time == "1":
+        await ctx.send(f"Listing info for ticker(s) every minute.")
+        await sched.add_job(All(), 'cron', minute = '*')
+    elif time == "5":
+        await ctx.send(f"Listing info for ticker(s) every {time} minutes.")
+        await sched.add_job(All(), 'cron', minute = '*/5')
+    elif time == "10":
+        await ctx.send(f"Listing info for ticker(s) every {time} minutes.")
+        await sched.add_job(All(), 'cron', minute = '*/10')
+    elif time == "15":
+        await ctx.send(f"Listing info for ticker(s) every {time} minutes.")
+        await sched.add_job(All(), 'cron', minute = '*/15')
+    elif time == "30":
+        await ctx.send(f"Listing info for ticker(s) every {time} minutes.")
+        await sched.add_job(All(), 'cron', minute = '*/30')
+    elif time == "60":
+        await ctx.send(f"Listing info for ticker(s) every hour.")
+        await sched.add_job(All(), 'cron', hour = '*')
+    else:
+        await ctx.send(f"{time} is not a valid input.")
 
-TOKEN = open("token.txt", "r").read()
+@bot.command()
+async def Graph(ctx):
+    await ctx.send("This command isn't implemented yet")
+
 bot.run(TOKEN)
--- TickerPriceBot/ticker_alert.py
+++ TickerPriceBot/ticker_alert.py
@@ -1,10 +1,11 @@
 import requests
 import json
 
+api_key = open("api_key.txt", "r").read()
+hook_url = open("hook_url.txt", "r").read()
+
 # Posts to Discord
 def postToDiscord(message):
-    
-    hook_url = open("hook_url.txt", "r").read()
     params = {
         "content" : message,
         "username" : "Ticker Alert"
@@ -14,29 +15,85 @@ def postToDiscord(message):
 
     print(post)
 
-# Make API Call
-def callAPI(ticker):
-    response = requests.get(f'https://api.cryptonator.com/api/ticker/{ticker.lower()}-usd')
-    response_json = response.json()
-    print(response)
-    print(response_json)
+# Make API Call For Stock Ticker
+def callStockAPI(ticker, mode):
+    results = []
+    ticker_list = []
 
+    if mode == "single":
+        ticker_list.append(ticker)
+    elif mode == "all":
+        ticker_list = [line.rstrip('\n') for line in open('tickers.txt')]
+    
+    # Alphavantage
+    for ticker in ticker_list:    
+        response = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={api_key}')
+        response_json = response.json()
+        print(response)
+        print(response_json)
+        price = float(response_json["Global Quote"]["05. price"])
+        results.append(f"The Price of {ticker} is: ${price:.2f}")
 
     # Posts if succesful 
-    if response.status_code == 200:
-        price = float(response_json["ticker"]["price"])
+    if response.status_code != 200:
+         #postToDiscord("Response code not 200: {}".format(response.status_code))
+        results = f"Response code not 200: {response.status_code}"
 
-        print(price)
-        #postToDiscord("BTC: ${}".format(price))
-        result = f"The Price of {ticker} is: ${price:.2f}"
-
-    else:
-        #postToDiscord("Response code not 200: {}".format(response.status_code))
-        result = f"Response code not 200: {response.status_code}"
+    # Converting List to Single stream for output
+    message = ""
+    for result in results:
+        if len(results) == 1:
+            message = str(result)
+        else:
+            message += (str(requests) + "\n")
     
-    return result
+    print(message)
+    
+    return message
 
+# Make API Call For Crypto Ticker
+def callCryptoAPI(crypto, mode):
+    results = []
+    crypto_list = []
 
+    if mode == "single":
+        crypto_list.append(crypto)
+        try:
+            response = requests.get(f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={crypto}&to_currency=USD&apikey={api_key}")
+            response_json = response.json()
+            print(response)
+            print(response_json)
+            price = float(response_json["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+            results.append(f"The Price of {crypto} is: ${price:.2f}")
+        except:
+            results.append(f"There was no price information for {crypto}.")
+
+    elif mode == "all":
+        crypto_list = [line.rstrip('\n') for line in open('cryptos.txt')]
+    
+    # Alphavantage
+    for crypto in crypto_list:    
+        try:
+            response = requests.get(f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={crypto}&to_currency=USD&apikey={api_key}")
+            response_json = response.json()
+            print(response)
+            print(response_json)
+            price = float(response_json["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+            results.append(f"The Price of {crypto} is: ${price:.2f}")
+        except:
+            results.append(f"There was no price information for {crypto}.")
+
+    # Converting List to Single stream for output
+    message = ""
+    for result in results:
+        if len(results) == 1:
+            message = str(result)
+        else:
+            message += (str(requests) + "\n")
+    
+    print(message)
+    
+    return message
 
 
 
